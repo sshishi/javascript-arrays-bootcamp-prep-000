81d630b373637ac624567536e441b231c7ca6be6
sshishi <khashi11@yahoo.com>
2017-10-07 16:06:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,4 +4,5 @@ var chocolateBars = [ "snickers" , "hundred grand" , "kitkat" , "skittles"]
 
 function addElementToBeginningOfArray (){
   ["Philadelphia", ...chocolateBars]
+  return chocolateBars
 }
