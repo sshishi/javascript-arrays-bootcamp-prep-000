5ac81347d6f9d162a0e35cd41e09fe9497f7826b
sshishi <khashi11@yahoo.com>
2017-10-07 15:46:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,13 +1,7 @@
 var chocolateBars = [ "snickers" , "hundred grand" , "kitkat" , "skittles"]
 
 
-function addElementToBeginningOfArray(){
-  chocolateBars.unshift("ppp")
-}
 
-function destructivelyAddElementToBeginningOfArray(){
-  var chocolateBars = ["Philadelphia",  ...chocolateBars]
-}
 function accessElementInArray(){
   console.log(chocolateBars[0])
 }
