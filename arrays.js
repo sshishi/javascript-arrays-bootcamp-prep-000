9e446c1a7e7951be870e04ffb9ed939ce689d10d
sshishi <khashi11@yahoo.com>
2017-10-07 14:31:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = [ "snickers" , "hundred grand" , "kitkat" , "skittles"]
 
 function addWlementToBeginningOfArray(){
-  ["cloth",...chocolateBars]
+  ["cloth", ...chocolateBars]
 }
