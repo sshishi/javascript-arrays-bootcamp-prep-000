8c8a710e0a69bb7856f6e862218dc99841f853b0
sshishi <khashi11@yahoo.com>
2017-10-07 14:17:01 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var chocolateBars = [ "snickers" , "hundred grand" , "kitkat" , "skittles"] ;
+var chocolateBars = new Array();
+
+function addWlementToBeginningOfArray(chocolateBars,y){
+  ["y", ...addWlementToBeginningOfArray]
+  return addWlementToBeginningOfArray
+}
+
+destructivelyAddElementToBeginningOfArray(chocolateBars,y){
+  ["y", ...addWlementToBeginningOfArray]
+  destructivelyAddElementToBeginningOfArray = ["y", ...addWlementToBeginningOfArray]
+  return addWlementToBeginningOfArray
+}
