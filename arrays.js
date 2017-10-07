4436d0e07a7ba91bb8728d831d9d3c475fb068e7
sshishi <khashi11@yahoo.com>
2017-10-07 15:27:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,10 +2,5 @@ var chocolateBars = [ "snickers" , "hundred grand" , "kitkat" , "skittles"]
 
 
 function addElementToBeginningOfArray(){
-    ["foo", ...chocolateBars]
-
-
-
-function destructivelyAddElementToBeginningOfArray(){
-  var chocolateBars = ["foo",  1]
+  [...cities, "Philadelphia"]
 }
