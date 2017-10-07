870f45d453ad271db01bad2b53e46d4fec06a5d2
sshishi <khashi11@yahoo.com>
2017-10-07 15:03:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = [ "snickers" , "hundred grand" , "kitkat" , "skittles"];
 
-funtion addElementToEndOfArray(){
+funtion addElementToEndOfArray(chocolateBars, element){
 
  ["cloth",  ...chocolateBars]
 }
